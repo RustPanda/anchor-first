8f22f5bcc48b2d5c369a053f05da5b9d79284727
Golubev Matvei <golubev_m@stilsoft.ru>
2022-03-29 20:48:17 +0300

    Update declare_id.

--- programs/first/src/lib.rs
+++ programs/first/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("BoeNUzUVRwqA4xUjL3dmoypZaFSJDMPz5MkWPL1gTZiK");
 
 #[program]
 pub mod first {
